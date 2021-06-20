39bdf4f2a4ca7e3c63fb966f51e8066d8550723e
Bruno <Bruno@DESKTOP-51F0SPF>
2021-06-20 13:28:57 -0300

    for e break

---
+++ JavaCaelum/src/capitulo03/For.java
@@ -0,0 +1,14 @@
+package capitulo03;
+
+public class For {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		for(int i = 0; i < 10 ; i++) { //inicialização ; condição ; incremento
+			System.out.println("Olá"); //repetirá o "Olá" 9 vezes
+		}
+
+	}
+
+}
