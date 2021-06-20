1fad8122dd2c26c0814267eda669a79b7e623de4
Bruno <Bruno@DESKTOP-51F0SPF>
2021-06-20 19:03:37 -0300

    break

--- JavaCaelum/src/capitulo03/Break.java
+++ JavaCaelum/src/capitulo03/Break.java
@@ -7,7 +7,7 @@ public class Break {
 		for (int i = 1; i < 100; i++ ) {
 			if (i % 19 == 0 ) {
 				System.out.println("Achei um divisivel por 19 entre 1 e 100");
-				break; //sem o break ele repetiria a mensagem 5 vezes.
+				break; //sem o break ele repetiria a mensagem 5 vezes!
 			}
 		}
 	}
